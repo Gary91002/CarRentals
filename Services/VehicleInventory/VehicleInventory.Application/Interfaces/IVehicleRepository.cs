5d3b874865df8fbdc5e541652200ad7e471a9d88
Gary91002 <sagoolul@gmail.com>
2026-02-17 12:14:07 -0500

    Defined IVehicleRepository

--- Services/VehicleInventory/VehicleInventory.Application/Interfaces/IVehicleRepository.cs
+++ Services/VehicleInventory/VehicleInventory.Application/Interfaces/IVehicleRepository.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VehicleInventory.Application.DTOs;
+using VehicleInventory.Domain.VehicleAggregate;
 
 namespace VehicleInventory.Application.Interfaces
 {
 	public interface IVehicleRepository
 	{
+		Task<Vehicle> GetById(int id);
+		Task<List<Vehicle>> GetAll();
+		Task Add(Vehicle vehicle);
+		Task Delete(int id);
+		Task SaveChanges();
+
+
 	}
 }
