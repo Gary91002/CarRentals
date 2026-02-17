20d9c8955e8c5d724812587cc04a27515b592b83
Gary91002 <sagoolul@gmail.com>
2026-02-17 00:06:30 -0500

    Created Enums and vehicle entity

---
+++ Services/VehicleInventory/VehicleInventory.Domain/Enums/VehicleType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleInventory.Domain.Enums
+{
+	public enum VehicleType
+	{
+		Car,
+		Truck,
+		Motorcycle,
+		SUV,
+		Van,
+		Coupe,
+		Convertible,
+		Hatchback,
+		Wagon
+	}
+}
---
+++ Services/VehicleInventory/VehicleInventory.Domain/VehicleAggregate/Vehicle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleInventory.Domain.Enums;
+
+namespace VehicleInventory.Domain.VehicleAggregate
+{
+	public class Vehicle
+	{
+		public int id { get; set; }
+		public string VehicleCode { get; set; } = string.Empty;
+		public int LocationId { get; set; }
+		public VehicleType VehicleType { get; set; }
+		public VehicleStatus VehicleStatus { get; set; }
+
+		public Vehicle(int id, string vehicleCode, int locationId, VehicleType vehicleType, VehicleStatus vehicleStatus)
+		{
+			this.id = id;
+			this.VehicleCode = vehicleCode;
+			this.LocationId = locationId;
+			this.VehicleType = vehicleType;
+			this.VehicleStatus = vehicleStatus;
+		}
+
+
+	}
+}
