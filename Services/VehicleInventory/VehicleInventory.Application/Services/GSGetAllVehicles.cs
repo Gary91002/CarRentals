50a5f933c7f6fa46f032089a3e2f90793faf22aa
Gary91002 <sagoolul@gmail.com>
2026-02-17 21:20:44 -0500

    Implemented GetAllVehiclesService

--- Services/VehicleInventory/VehicleInventory.Application/Services/GSGetAllVehicles.cs
+++ Services/VehicleInventory/VehicleInventory.Application/Services/GSGetAllVehicles.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VehicleInventory.Application.DTOs;
+using VehicleInventory.Application.Interfaces;
 
 namespace VehicleInventory.Application.Services
 {
 	public class GSGetAllVehicles
 	{
+		private readonly IVehicleRepository _vehicleRepository;
+
+		public GSGetAllVehicles(IVehicleRepository vehicleRepository)
+		{
+			_vehicleRepository = vehicleRepository;
+		}
+
+		public async Task<List<GSVehicleDto>> GetAllVehicles()
+		{
+			var vehicles = await _vehicleRepository.GetAll();
+			return vehicles.Select(vehicle => new GSVehicleDto
+			{
+				Id = vehicle.Id,
+				VehicleCode = vehicle.VehicleCode,
+				LocationId = vehicle.LocationId,
+				VehicleType = vehicle.VehicleType,
+				VehicleStatus = vehicle.VehicleStatus
+			}).ToList();
+		}
+
 	}
 }
