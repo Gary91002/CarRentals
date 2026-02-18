879c26d13253ac5d1f6349e6eb10d934a951470e
Gary91002 <sagoolul@gmail.com>
2026-02-17 22:44:37 -0500

    Implemented VehicleRepository

--- Services/VehicleInventory/VehicleInventory.Infrastructure/Repositories/GSVehicleRepository.cs
+++ Services/VehicleInventory/VehicleInventory.Infrastructure/Repositories/GSVehicleRepository.cs
@@ -1,12 +1,53 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VehicleInventory.Application.Interfaces;
+using VehicleInventory.Domain.VehicleAggregate;
+using VehicleInventory.Infrastructure.Data;
 
 namespace VehicleInventory.Infrastructure.Repositories
 {
-	internal class GSVehicleRepository
+	public class GSVehicleRepository : IVehicleRepository
 	{
+		private readonly GSInventoryDbContext _context;
+
+		public GSVehicleRepository(GSInventoryDbContext context)
+		{
+			_context = context;
+		}
+
+		public async Task Add(GSVehicle vehicle)
+		{
+			await _context.Vehicles.AddAsync(vehicle);
+			await SaveChanges();
+		}
+
+		public async Task Delete(int id)
+		{
+			var vehicle = await _context.Vehicles.FindAsync(id);
+			if (vehicle != null)
+			{
+				_context.Vehicles.Remove(vehicle);
+				await SaveChanges();
+			}
+		}
+
+		public async Task<List<GSVehicle>> GetAll()
+		{
+			return await _context.Vehicles.ToListAsync();
+		}
+
+		public async Task<GSVehicle> GetById(int id)
+		{
+			return await _context.Vehicles.FindAsync(id);
+		}
+
+		public async Task SaveChanges()
+		{
+			await _context.SaveChangesAsync();
+		}
 	}
 }
