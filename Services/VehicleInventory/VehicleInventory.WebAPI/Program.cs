a076651713616df76e7068d6c67d0308af818a91
Gary91002 <sagoolul@gmail.com>
2026-02-17 23:31:21 -0500

    Updated porgram.cs and created migrations

--- Services/VehicleInventory/VehicleInventory.WebAPI/Program.cs
+++ Services/VehicleInventory/VehicleInventory.WebAPI/Program.cs
@@ -1,7 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using VehicleInventory.Application.Interfaces;
+using VehicleInventory.Application.Services;
+using VehicleInventory.Infrastructure.Data;
+using VehicleInventory.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<GSInventoryDbContext>(options =>
+	options.UseSqlServer(builder.Configuration.GetConnectionString("Server=(localdb)\\MSSQLLocalDB;Database=VehicleInventory;Trusted_Connection=True;TrustServerCertificate=True;MultipleActiveResultSets=true")));
+builder.Services.AddScoped<IVehicleRepository, GSVehicleRepository>();
+builder.Services.AddScoped<GSCreateVehicle>();
+builder.Services.AddScoped<GSGetAllVehicles>();
+builder.Services.AddScoped<GSGetVehicleById>();
+builder.Services.AddScoped<GSUpdateVehicleStatus>();
+builder.Services.AddScoped<GSDeleteVehicle>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
