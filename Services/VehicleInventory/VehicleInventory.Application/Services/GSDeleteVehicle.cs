31dcde556230ca0dacceea21f20daa8abaad416c
Gary91002 <sagoolul@gmail.com>
2026-02-17 21:52:49 -0500

    Implemented DeleteVehicle Service

--- Services/VehicleInventory/VehicleInventory.Application/Services/GSDeleteVehicle.cs
+++ Services/VehicleInventory/VehicleInventory.Application/Services/GSDeleteVehicle.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VehicleInventory.Application.Interfaces;
 
 namespace VehicleInventory.Application.Services
 {
-	internal class GSDeleteVehicle
+	public class GSDeleteVehicle
 	{
+		private readonly IVehicleRepository _vehicleRepository;
+
+		public GSDeleteVehicle(IVehicleRepository vehicleRepository)
+		{
+			_vehicleRepository = vehicleRepository;
+		}
+
+		public async Task DeleteVehicle(int id)
+		{
+			var vehicle = await _vehicleRepository.GetById(id);
+			// GetVehicleById already checks for null so i dont need it again.
+
+			await _vehicleRepository.Delete(id);
+			await _vehicleRepository.SaveChanges();
+		}
+
+
 	}
 }
