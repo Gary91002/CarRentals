17160ac815b4d1e9d4c1809e4b5e00496e5064c3
Gary91002 <sagoolul@gmail.com>
2026-02-17 21:42:02 -0500

    Implemented UpdateVehicleStatus Service

--- Services/VehicleInventory/VehicleInventory.Application/Services/GSUpdateVehicleStatus.cs
+++ Services/VehicleInventory/VehicleInventory.Application/Services/GSUpdateVehicleStatus.cs
@@ -3,10 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VehicleInventory.Application.DTOs;
+using VehicleInventory.Application.Interfaces;
+using VehicleInventory.Domain.Enums;
 
 namespace VehicleInventory.Application.Services
 {
-	internal class GSUpdateVehicleStatus
+	public class GSUpdateVehicleStatus
 	{
+		private readonly IVehicleRepository _vehicleRepository;
+
+		public GSUpdateVehicleStatus(IVehicleRepository vehicleRepository)
+		{
+			_vehicleRepository = vehicleRepository;
+		}
+
+		public async Task<GSVehicleDto> UpdateVehicleStatus(int id, VehicleStatus newStatus)
+		{
+			var currentVehicle = await _vehicleRepository.GetById(id);
+
+			if (currentVehicle == null)
+			{
+				throw new Exception($"Vehicle with id {id} not found.");
+			}
+			switch (newStatus)
+			{
+				case VehicleStatus.Available:
+					currentVehicle.MarkAvailable();
+					break;
+				case VehicleStatus.Rented:
+					currentVehicle.MarkRented();
+					break;
+				case VehicleStatus.Reserved:
+					currentVehicle.MarkReserved();
+					break;
+				case VehicleStatus.Maintenance:
+					currentVehicle.MarkServiced();
+					break;
+				default:
+					throw new Exception($"Invalid vehicle status: {newStatus}");
+			}
+
+			await _vehicleRepository.SaveChanges();
+
+			return new GSVehicleDto
+			{
+				Id = currentVehicle.Id,
+				VehicleCode = currentVehicle.VehicleCode,
+				LocationId = currentVehicle.LocationId,
+				VehicleType = currentVehicle.VehicleType,
+				VehicleStatus = currentVehicle.VehicleStatus
+			};
+
+
+
+		}
+
 	}
 }
